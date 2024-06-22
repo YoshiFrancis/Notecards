d5bb926262020740ba29aa28a242ddddcdb5a640
YoshiFrancis <francis.ozua@gmail.com>
2024-06-22 17:14:27 -0500

    iniital settup of frontend

---
+++ Notecards/src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Note the Cards</h1>
+      <p>Website for notecards, notecards, and notecards! <i>Write that down!</i></p>
+    </div>
+  )
+}
+
+export default Home;
