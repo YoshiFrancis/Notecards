c77c5d5abcdf17b5f7b7c4645671f01d57316a89
YoshiFrancis <francis.ozua@gmail.com>
2024-06-23 17:11:30 -0500

    implemented regular login handler. no tests yet

--- backend/api/users.go
+++ backend/api/users.go
@@ -8,7 +8,24 @@ import (
 
 func (s *Server) postLoginHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		decoder := json.NewDecoder(req.Body)
+		var user User
 
+		if err := decoder.Decode(&user); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		var password string
+		err := s.dbpool.QueryRow(context.Background(), "select password from users where username=$1", user.Password_hash).Scan(&password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if password != "" {
+			w.WriteHeader(http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
 	}
 }
 
@@ -22,7 +39,7 @@ func (s *Server) postNewLoginHandler() http.HandlerFunc {
 			return
 		}
 		var username string
-		err := s.dbpool.QueryRow(context.Background(), "select name from widgets where username=$1", user.Username).Scan(&username)
+		err := s.dbpool.QueryRow(context.Background(), "select username from users where username=$1", user.Username).Scan(&username)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
